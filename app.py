c59e4e7a534edf31c746278083e7567893a620fd
Edward Scissorhands <edward_beats@hotmail.com>
2017-11-07 13:18:22 -0600

    hope this works

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
+import os
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, request
-from helpers.util import build_dsn
-from config.database import config as dbconfig
+if os.environ.get('DATABASE') is None:
+	from helpers.util import build_dsn
+	from config.database import config as dbconfig
 
 # Resources
 from resources.users import User,Register,Auth,ProtectedAccess
 from resources.home import Home
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = build_dsn(
-	dbconfig['host'],
-	dbconfig['user'],
-	dbconfig['password'],
-	dbconfig['database']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 
+	build_dsn(
+		dbconfig['host'],
+		dbconfig['user'],
+		dbconfig['password'],
+		dbconfig['database']
+	)
 )
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'MySecret'
